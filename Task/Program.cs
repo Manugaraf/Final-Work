f64c0709b5cdada11562d9163e575712b4cd0525
Dmitrii <petrovoo096@gmail.com>
2023-01-28 19:36:38 +0300

    Написал метод

--- Task/Program.cs
+++ Task/Program.cs
@@ -7,3 +7,14 @@ for (int i = 0; i < n; i++)
     Console.Write($"Введите {i + 1} элемент: ");
     array[i] = Convert.ToString(Console.ReadLine());
 }
+
+Console.WriteLine();
+for (int j = 0; j < n; j++)
+{
+    int m = 3;
+    int element = array[j].Length;
+    if (element <= m)
+    {
+        Console.Write($"{array[j]} ");
+    } 
+}
