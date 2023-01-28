2bcada7d173f48af9062ad43a584413ce1981fe9
Dmitrii <petrovoo096@gmail.com>
2023-01-28 19:10:42 +0300

    Добавил ввод пользователем

---
+++ Task/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите кол-во сколько будет элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите {i + 1} элемент: ");
+    array[i] = Convert.ToString(Console.ReadLine());
+}
